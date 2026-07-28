b938f7766402aad90bf90c4cbe0263529144c6cb
UberMetroid <UberMetroid@users.noreply.github.com>
2026-07-28 08:34:01 -0700

    chore(release): bump version to v0.3.0-alpha (30:1 Gold Standard Ratio Milestone)

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ use codegen::LlvmCodeGen;
 #[derive(ClapParser)]
 #[command(name = "ooda")]
 #[command(author = "openOODA Core Team")]
-#[command(version = "0.2.6-alpha")]
+#[command(version = "0.3.0-alpha")]
 #[command(about = "The OODA Programming Language Compiler & Toolchain", long_about = None)]
 struct Cli {
     #[command(subcommand)]
